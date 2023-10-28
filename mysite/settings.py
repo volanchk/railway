57ae0a5cfa3f5882b1486f9ab64a67dc39da9a02
Washkevich Alexey <72569424+volanchk@users.noreply.github.com>
2023-10-28 21:47:58 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://django-server-production-51d9.up.railway.app/", '127.0.0.1']
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
