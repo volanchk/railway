47f30e79a0f0d109bc6acba6668a5d55fd51ced4
Washkevich Alexey <72569424+volanchk@users.noreply.github.com>
2023-10-28 22:06:41 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ["PGDATABASE"],
         'USER': os.environ["PGUSER"],
         'PASSWORD': os.environ["PGPASSWORD"],
