e5312127b5d2522d6b43b6504bc3cb693e7daaa5
Washkevich Alexey <72569424+volanchk@users.noreply.github.com>
2023-11-08 20:42:48 +0300

    Update settings.py

--- bestbook/settings.py
+++ bestbook/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
+DJANGO_SUPERUSER_USERNAME = os.environ['DJANGO_SUPERUSER_USERNAME']
+DJANGO_SUPERUSER_PASSWORD = os.environ['DJANGO_SUPERUSER_PASSWORD']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
